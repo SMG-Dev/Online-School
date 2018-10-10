6a4045e92abdfe844e71ace71bcfc0b0a63e90d4
Alex Tsvetanov <alex@alexts.tk>
2018-10-10 03:55:53 +0300

    some edits not finished side bar with news

---
+++ lib/mysql.php
@@ -0,0 +1,37 @@
+<?php
+	class MySQL
+	{
+		public  $servername = "localhost";
+		private $username = "root";
+		private $password = "tts2002";
+		
+		public $dbname;
+		public $conn;
+		
+		function __construct (string $dbname_param)
+		{
+			$this->dbname = $dbname_param;
+			$this->conn = new mysqli ($this->servername, $this->username, $this->password, $this->dbname);
+			if ($this->conn->connect_error) 
+			{
+				die("Connection failed: " . $this->conn->connect_error);
+			}
+		}
+		
+		function __destruct ()
+		{
+			$this->conn->close ();
+		}
+
+		function prepare (string $command)
+		{
+			return ($this->conn->prepare($command));
+		}
+		
+		function query (string $command)
+		{
+			return ($this->conn->query ($command));
+		}
+	}
+?>
+
